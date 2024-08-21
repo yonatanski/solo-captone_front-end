f3bac01ca9f74234812ba9274882dcb823df0486
Yonatan Gosaye <88892819+yonatanski@users.noreply.github.com>
2024-08-21 15:03:04 +0200

    Test for order history page
    Breaer token is saying undfined

--- capstone_app-fe/src/ReqMethod.js
+++ capstone_app-fe/src/ReqMethod.js
@@ -1,7 +1,10 @@
 import axios from "axios"
 // const BASE_URL = "http://localhost:3050/api/"
 const BASE_URL = `${process.env.REACT_APP_BE_URL}/api/`
-const TOKEN = localStorage.getItem("persist:root") ? JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user).currentUser?.accessToken : undefined
+const TOKEN = localStorage.getItem("persist:root")
+  ? JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)
+      .currentUser?.accessToken
+  : undefined
 console.log(TOKEN)
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
@@ -9,5 +12,7 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  headers: { Authorization: `Bearer ${TOKEN}` },
+  headers: {
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjczMjgyMDg0N2RlZWNhOGRkMDAyM2IiLCJyb2xlIjp0cnVlLCJpYXQiOjE3MjQyNDM5NzEsImV4cCI6MTcyNDg0ODc3MX0.stj8fFUB1yFgUlQMlhAmYFqRTDhEw3GR9O2VPh5u-Z0`,
+  },
 })
